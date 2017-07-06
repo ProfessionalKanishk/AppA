cf42f0a3eaa0f353b2daf5db57ad202578094754
cowslayer982 <k.singh@utexas.edu>
2017-07-05 19:45:24 -0500

    poop

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,24 @@
+#clear table
+
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
+
+db= MySQLdb.connect(user='root',password='s1101419',host='159.203.127.147',database='food_test',port='3306')
+
+cursor= db.cursor()
+
+cursor.execute("TRUNCATE TABLE free_fud")
+
+db.close()
+
+#update table
+
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import mysql.connector
+
+
 
 
 my_url = 'https://calendar.utexas.edu/calendar/day/2016/8/30'
@@ -9,7 +27,7 @@ my_url = 'https://calendar.utexas.edu/calendar/day/2016/8/30'
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
-cnx = mysql.connector.connect(user='root',password='s1101419',host='127.0.0.1',database='test_server')
+cnx = mysql.connector.connect(user='root',password='s1101419',host='159.203.127.147',database='food_test',port='3306')
 
 cursor = cnx.cursor()
 
@@ -27,31 +45,9 @@ for container in containers:
 	str2 = "free pizza"
 	lines = event.split('\n')
 	
-	print ('|||||'.join(lines))
 	
 	if any(str1 in s for s in lines):
-		print("***********************")
-		event_title = container.findAll("h3",{"class":"summary"})
-		event_name = event_title.find('a').text
-	
-		time_html = container.findAll("div",{"class":"dateright"})
-		time = time_html[0].text.strip()
-		
-		location_html = container.findAll("div",{"class":"location"})
-		location = location_html[0].text.strip()
-		
-		event_title = str(event_title)
-		print ("event name: "+ event_title)
-		print ("food: food?")
-		print ("location: "  + location)
-		print ("time: " + time)
-		
-		#cursor.execute("INSERT INTO food_table VALUES (%s, %s, %s, %s)", (event,food,location,time))
-		#increment = cursor.lastrowid # not sure if this does much, but was recommended to make data neater
-	
-		cnx.commit()
-	elif any(str2 in s for s in lines):
-		print("*************************")
+		#print("***********************")
 		event_title = container.findAll("h3",{"class":"summary"})
 		event_name = event_title[0].text.strip()
 	
@@ -62,21 +58,46 @@ for container in containers:
 		location = location_html[0].text.strip()
 		
 		event_title = str(event_title)
-		print ("event name: "+ event_title)
-		print ("food: pizza")
+		
+		food = "N/A"
+		print ("event name: "+ event_name)
+		print ("food: "+food)
 		print ("location: "  + location)
 		print ("time: " + time)
 		
-		#cursor.execute("INSERT INTO food_table VALUES (%s, %s, %s, %s)", (event,food,location,time))
-		#increment = cursor.lastrowid # not sure if this does much, but was recommended to make data neater
+		cursor.execute("INSERT INTO free_fud VALUES (%s, %s, %s, %s)", (event_name,food,location,time))
+		increment = cursor.lastrowid # not sure if this does much, but was recommended to make data neater
 	
-		cnx.commit()
-
+		print("********************")
+	elif any(str2 in s for s in lines):
+		#print("*************************")
+		event_title = container.findAll("h3",{"class":"summary"})
+		event_name = event_title[0].text.strip()
+	
+		time_html = container.findAll("div",{"class":"dateright"})
+		time = time_html[0].text.strip()
+		
+		location_html = container.findAll("div",{"class":"location"})
+		location = location_html[0].text.strip()
+		
+		event_title = str(event_title)
+		
+		food = "pizza"
+		print ("event name: "+ event_name)
+		print ("food: "+ food)
+		print ("location: "  + location)
+		print ("time: " + time)
+		
+		cursor.execute("INSERT INTO free_fud VALUES (%s, %s, %s, %s)", (event_name,food,location,time))
+		increment = cursor.lastrowid # not sure if this does much, but was recommended to make data neater
+	
+		print("********************")
 	
 	else:
 	#do nothing
 		g=0
 
+		
 cnx.commit()
 
 cursor.close()
